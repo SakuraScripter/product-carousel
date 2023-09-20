cdc3e3426cdf97cd5ad6f235bffbed93ba9cfaeb
catia brehm <catiaejbrehm@gmail.comm>
2023-09-20 18:24:13 +0100

    data changes

--- src/data.js
+++ src/data.js
@@ -19,12 +19,14 @@ export const responsive = {
 
 export const productData = [
   {
+    item: 1,
     imageurl:
       "https://plus.unsplash.com/premium_photo-1674129670339-554be9059cf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8U2tpcnRzfGVufDB8fDB8fHww&auto=format&fit=crop&w=1400&q=60",
     name: "Skirt",
     price: "Price: 25,99",
   },
   {
+    item: 2,
     imageurl:
       "https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QmFnfGVufDB8fDB8fHww&auto=format&fit=crop&w=1400&q=60",
     name: "Bag",
@@ -38,30 +40,35 @@ export const productData = [
     price: "Price: 154,23",
   },
   {
+    item: 4,
     imageurl:
       "https://images.unsplash.com/photo-1591117105338-4eb266b13c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QmVsdHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1400&q=60",
     name: "Belt",
     price: "Price: 154,23",
   },
   {
+    item: 5,
     imageurl:
       "https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2hpcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1400&q=60",
     name: "Shirt",
     price: "Price: 154,23",
   },
   {
+    item: 6,
     imageurl:
       "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8U3VuZ2xhc3Nlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1400&q=60",
     name: "Sunglasses",
     price: "Price: 59,45",
   },
   {
+    item: 7,
     imageurl:
       "https://images.pexels.com/photos/52518/jeans-pants-blue-shop-52518.jpeg",
     name: "Pants",
     price: "Price: 38,59",
   },
   {
+    item: 8,
     imageurl:
       "https://images.pexels.com/photos/4046316/pexels-photo-4046316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     name: "Cream",
